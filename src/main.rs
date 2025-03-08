336f6ab86939f50169d8b13a3fa021c858491642
Linus Walker <rissk_it@proton.me>
2025-03-08 22:13:55 +0630

    chore: iced

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::ops::Deref;
-
 use iced::widget::{Column, Container, Image, Row, column, container, row, scrollable, text};
 use iced::{Alignment, Element, Length, Theme};
 
@@ -25,16 +23,26 @@ impl MainUi {
     const FONT: &'static [u8] = include_bytes!("/usr/share/fonts/TTF/ZedMonoNerdFont-Regular.ttf");
 
     pub fn view(&self) -> Element<Message> {
-        let posts: Vec<Element<Message>> = (0..30)
-            .map(|_| container(self.body()).padding(5).into())
-            .collect();
+        let posts: Vec<u8> = (0..30).collect();
+        let post_per_row: Vec<Row<Message>> =
+            posts.chunks(3).map(|v| row![self.body(&v[0])]).collect();
+        let mut tp = 0;
         let post_column = scrollable(
-            row((0..3).map(|_| {
-                column(posts.iter().enumerate().map(|(i, _)| posts[i]))
-                    .width(Length::Fill)
-                    .into()
-            }))
-            .spacing(10),
+            column((0..(posts.len() - tp)).map(|i| {
+                row((0..3).map(|_| {
+                    let body = self.body(&posts[tp]).into();
+                    if tp < posts.len() - 1 {
+                        tp += 1;
+                    }
+                    body
+                }))
+                .into()
+            })), // row((0..3).map(|_| {
+                 //     column(posts.iter().map(|i| self.body(i).into()))
+                 //         .width(Length::Fill)
+                 //         .into()
+                 // }))
+                 // .spacing(10),
         )
         .width(Length::Fill)
         .height(Length::Fill);
@@ -64,13 +72,13 @@ impl MainUi {
             .padding(10)
     }
 
-    pub fn body(&self) -> Column<Message> {
+    pub fn body(&self, texts: &u8) -> Column<Message> {
         let image = Image::new("/home/walker/github/dotfiles/Wallpapers/buddha.jpg")
             .width(300)
             .height(150)
             .content_fit(iced::ContentFit::Fill);
 
-        let text_content = text("Hello World!").size(20);
+        let text_content = text(texts).size(20);
 
         let post = container(column![image, text_content].spacing(10).padding(10))
             .width(320)
